78f5c3cb20090f5960aad0e02b07a38af68cd773
kristina deeva <druzhininaki@mail.ru>
2023-02-17 18:51:42 +0300

    DZ 2

---
+++ 18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    System.Console.WriteLine(number % 10);
+
+}
+else
+{
+    System.Console.WriteLine("Данное число не верно");
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    System.Console.WriteLine(number / 10 % 10);
+}
+else 
+{
+    System.Console.WriteLine("Данное число не верно");
+}
